165084e19f373d60cdb2bfad6626facf285e7332
shxdowlynnie <thirdeyebikes@gmail.com>
2026-04-19 11:02:54 -0300

    Actualizar index.js

--- index.js
+++ index.js
@@ -37,12 +37,12 @@ const PORT = process.env.PORT || process.env.SERVER_PORT || 3000
 
 let { say } = cfonts
 console.log(chalk.magentaBright('\n🌾 Iniciando...'))
-say('ShadowBot', {
+say('Tae-haa Bot', {
 font: 'simple',
 align: 'left',
 gradient: ['yellow', 'white']
 })
-say('Made with Yosue', {
+say('Made with Adara', {
 font: 'console',
 align: 'center',
 colors: ['red', 'magenta', 'yellow']
