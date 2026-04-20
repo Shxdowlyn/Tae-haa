60154444f3ca7d3e8477e1e4907535cad1903cd9
shxdowlynnie <thirdeyebikes@gmail.com>
2026-04-20 14:51:48 -0300

    Actualizar economia-slot.js

--- plugins/economia-slot.js
+++ plugins/economia-slot.js
@@ -75,13 +75,15 @@ resultado = `✿┏━━━━━━━━━━━━━━┓✿
 users.coin -= apuesta
 }
 users.lastslot = Date.now()
-const finalText = `「✿」| *SLOTS* 
-────────
-${x[0]} : ${y[0]} : ${z[0]}
-${x[1]} : ${y[1]} : ${z[1]}
-${x[2]} : ${y[2]} : ${z[2]}
-────────
-${resultado}`
+const finalText = `✿┏━━━━━━━━━━━━━━┓✿
+┃ 🎰 𝐒𝐋𝐎𝐓𝐒
+┣━━━━━━━━━━━━━━┫
+┃ ${x[0]} : ${y[0]} : ${z[0]}
+┃ ${x[1]} : ${y[1]} : ${z[1]}
+┃ ${x[2]} : ${y[2]} : ${z[2]}
+┣━━━━━━━━━━━━━━┫
+${resultado}
+✿┗━━━━━━━━━━━━━━┛✿`
 await conn.sendMessage(m.chat, { text: finalText, edit: key }, { quoted: m })
 }
 
