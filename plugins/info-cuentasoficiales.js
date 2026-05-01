25300369ee08b35df96888ed9d0215ead816c373
shxdowlynnie <thirdeyebikes@gmail.com>
2026-05-01 11:05:59 -0300

    Actualizar info-cuentasoficiales.js

--- plugins/info-cuentasoficiales.js
+++ plugins/info-cuentasoficiales.js
@@ -13,28 +13,37 @@ let handler = async (m, {conn, command}) => {
     participant: '0@s.whatsapp.net'
   }
 
-  let str = `🌑⚔️ 𝘽𝙄𝙀𝙉𝙑𝙀𝙉𝙄𝘿𝙊(𝘼) 𝘼 𝙇𝘼𝙎 𝘾𝙐𝙀𝙉𝙏𝘼𝙎 𝙊𝙁𝙄𝘾𝙄𝘼𝙇𝙀𝙎 ⚔️🌑
-💜 𝙒𝙀𝙇𝘾𝙊𝙈𝙀 𝙏𝙊 𝙏𝙃𝙀 𝙊𝙁𝙁𝙄𝘾𝙄𝘼𝙇 𝘼𝘾𝘾𝙊𝙐𝙉𝙏𝙎
-┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-💖 𝙎𝙝𝙖𝙙𝙤𝙬-𝘽𝙊𝙏-𝙈𝘿 ⚔️✨
-『☽』 El poder oculto se revela solo en las sombras...
-┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-✅ *GITHUB OFICIAL*
-https://github.com/yosue891
-┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-✅ *CANAL OFICIAL - YOSUE*
-https://whatsapp.com/channel/0029VbArz9fAO7RGy2915k3O
-┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-✅ *CANAL OFICIAL - ADO*
-https://whatsapp.com/channel/0029VbBIgz1HrDZg92ISUl2M
-┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-✅ *COMUNIDAD OFICIAL*
-https://chat.whatsapp.com/ETHW7aP7kOICrR2RBrfE6N
-┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-👑 Creador Principal: +58 424-2773183 (Yosue)
-⚔️ Segundo Creador: +504 9373-2693 (Ado)
-┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-🌌✨ *Shadow-BOT-MD* — El jardín sombrío nunca duerme ✨🌌`
+  let str = `╭━━━〔 BIENVENIDO 〕━━━╮
+┃
+┃ 🌑⚔️ CUENTAS OFICIALES ⚔️🌑
+┃ 💜 Welcome to Official Accounts
+┃
+┣━━━〔 BOT 〕━━━┫
+┃ ✦ Shadow-BOT-MD
+┃   El poder oculto se revela
+┃   solo en las sombras...
+┃
+┣━━━〔 LINKS OFICIALES 〕━━━┫
+┃ GitHub:
+┃ https://github.com/yosue891
+┃
+┃ Canal Yosue:
+┃ https://whatsapp.com/channel/0029VbArz9fAO7RGy2915k3O
+┃
+┃ Canal Ado:
+┃ https://whatsapp.com/channel/0029VbBIgz1HrDZg92ISUl2M
+┃
+┃ Comunidad:
+┃ https://chat.whatsapp.com/ETHW7aP7kOICrR2RBrfE6N
+┃
+┣━━━〔 CREADORES〕━━━┫
+┃ ✦ Yosue
+┃   +58 424-2773183
+┃
+┃ ✦ Ado
+┃   +504 9373-2693
+┃
+╰━━━〔 SISTEMA ACTIVO 〕━━━╯`
 
   await conn.sendButton(
     m.chat,
