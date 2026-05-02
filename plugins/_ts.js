4d7c2284c3651cf1fe752423e32f6e3ba9503dad
shxdowlynnie <thirdeyebikes@gmail.com>
2026-05-02 15:20:26 -0300

    Actualizar _ts.js

--- plugins/_ts.js
+++ plugins/_ts.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn }) => {
   try {
     exec("df -h / && df -i /", (err, stdout) => {
       if (err) return conn.reply(m.chat, "⚠ Error al revisar disco", m)
-      conn.reply(m.chat, "📊 Estado del servidor:\n\n" + stdout, m)
+      conn.reply(m.chat, "Estado del servidor:\n\n" + stdout, m)
     })
 
     let tempPath = "./tmp"
@@ -14,17 +14,17 @@ let handler = async (m, { conn }) => {
       for (let file of files) {
         fs.unlinkSync(`${tempPath}/${file}`)
       }
-      conn.reply(m.chat, "🧹 Carpeta temp limpiada ✅", m)
+      conn.reply(m.chat, "Carpeta temp limpiada", m)
     }
 
     if (fs.existsSync("/tmp")) {
       exec("rm -rf /tmp/*", (err) => {
-        if (!err) conn.reply(m.chat, "🧹 Carpeta /tmp limpiada ✅", m)
+        if (!err) conn.reply(m.chat, "Carpeta /tmp limpiada", m)
       })
     }
   } catch (e) {
     console.error(e)
-    conn.reply(m.chat, "⚠ Se produjo un error al limpiar espacio.", m)
+    conn.reply(m.chat, "Se produjo un error al limpiar espacio.", m)
   }
 }
 
